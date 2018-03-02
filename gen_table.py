b27e55b0cdfa86f4b376033d50fbce72b26905dc
root <root@localhost.localdomain>
2018-03-02 20:30:58 +0000

    added rainbow to reduction

--- gen_table.py
+++ gen_table.py
@@ -5,18 +5,20 @@ from Crypto.Cipher import AES
 import sys
 import binascii
 import os
+import random
 
 digits = sys.argv[1]
 n = int(digits)
 chars = n // 4
 
 
-def reduc(hsh):
-    plain = hsh[0:chars-1]
-    return plain
+def reduc(hsh, col):
+    plain = hsh[0:chars]
+    plainint = (int(plain, 16) + col) % (2**n)
+    return ((str(hex(plainint))[2:]).zfill(chars)).replace("L", "")
 
 def makeHash(plain):
-    key = binascii.unhexlify(("0"*(32-chars)) + ("0" * (chars-len(str(plain)))) + str(plain))
+    key = binascii.unhexlify(("0"*(32-chars)) + str(plain))
     IV = 16 * '\x00'
     #IV = os.urandom(16)
     encryptor = AES.new(key, AES.MODE_CBC, IV=IV)
@@ -24,6 +26,7 @@ def makeHash(plain):
     return binascii.hexlify(encryptor.encrypt("\x00"*16))
 
 def gen_table():
+    random.seed(16)
     length = 2**(n // 2)
     tablesize = length
     starts = []
@@ -32,16 +35,18 @@ def gen_table():
     depth = 0
     while depth < tablesize:
         count = 0
-        plain = 4 * depth
+        plain = (str(hex(random.randrange(0,2**n)))[2:]).zfill(chars)
+        plain = plain.replace("L", "")
+        start = plain
         while count < length:
             hsh = makeHash(plain)
-            plain = reduc(hsh)
+            plain = reduc(hsh,count)
             count += 1
-        starts.append(depth * 4)
+        starts.append(start)
         ends.append(makeHash(plain))
         depth += 1
     return [starts,ends]
-print makeHash("ABC") 
+print makeHash("ABCABC") 
 data = gen_table()
 f = open("rainbow", "w");
 for i in range(len(data[0])):
